f448ee227195aed7c5439101d5528db4bf7eeeec
pikachris <chris.mithe2014@gmail.com>
2020-07-06 17:57:29 +0100

    added missing files

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,37 @@
-#Download base image ubuntu 18.04
-FROM ubuntu:18.04
+#Download base image python:3.6-stretch
+FROM python:3.6-stretch
 
-LABEL maintainer="jannetta.steyn@newcastle.ac.uk"
+LABEL maintainer="C.Deligiorgi@newcastle.ac.uk"
 
 ENV TZ=Europe/London
 
-# Update Ubuntu Software repository
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-RUN apt-get update
-RUN apt-get upgrade -y
-
-# Install basic utilities and python
-RUN apt-get -y install apt-utils
-RUN apt-get -y install vim python3.6 python3.6-dev python3-pip wget 
-RUN apt-get update
-RUN apt-get upgrade -y
-
 # Install Python 3 libraries
-RUN pip3 install cython jupyter jupyter-core notebook sklearn pivottablejs GPyOpt pandas
-RUN python3 -m pip install tensorflow
+RUN pip install cython jupyter jupyter-core notebook sklearn pivottablejs GPyOpt pandas
+RUN pip install tensorflow
+RUN pip install numpy scipy
+RUN pip install --find-links https://download.pytorch.org/whl/torch_stable.html torch==1.3.1 torchvision==0.4.2
+RUN pip install tqdm
 
 # Copy configuration file with password
 RUN mkdir /root/.jupyter
 COPY jupyter_notebook_config.py /root/.jupyter/.
 
-# Create volume for persistency
-VOLUME JupyterNotes
+# Define the working directory
 WORKDIR /JupyterNotes
+
+# Copy over python scripts
+COPY train3.py /JupyterNotes
+COPY convnet3.py /JupyterNotes
+COPY dataset2.py /JupyterNotes
+
+RUN mkdir /JupyterNotes/red.png && mkdir /JupyterNotes/green.png
+COPY red.png /JupyterNotes/red.png
+COPY green.png /JupyterNotes/green.png
+
+
 EXPOSE 8080
 
 RUN cd /JupyterNotes
+
 # Run command to keep container running
 CMD cd ; jupyter notebook --port=8080 --no-browser --ip=0.0.0.0 --allow-root
--- train3.py
+++ train3.py
@@ -20,14 +20,18 @@ from convnet3 import Convnet
 from dataset2 import CellsDataset
 
 parser = argparse.ArgumentParser('Predicting hits from pixels')
-parser.add_argument('name',type=str,help='Name of experiment')
-parser.add_argument('data_dir',type=str,help='Path to data directory containing images and gt.csv')
-parser.add_argument('--num_steps',type=int,default=20000,help='Number of training iterations')
-parser.add_argument('--batchsize',type=int,default=16,help='Size of batch')
-parser.add_argument('--weight_decay',type=float,default=0.0,help='Weight decay coefficient (something like 10^-5)')
-parser.add_argument('--lr',type=float,default=0.0001,help='Learning rate')
-parser.add_argument('--resume',action='store_true',help='Resume experiments from checkpoint directory')
-parser.add_argument('--seed',type=int,default=1337,help='RNG seed')
+parser.add_argument('name', type=str, help='Name of experiment')
+parser.add_argument('data_dir', type=str,
+                    help='Path to data directory containing images and gt.csv')
+parser.add_argument('--num_steps', type=int, default=20000,
+                    help='Number of training iterations')
+parser.add_argument('--batchsize', type=int, default=16, help='Size of batch')
+parser.add_argument('--weight_decay', type=float, default=0.0,
+                    help='Weight decay coefficient (something like 10^-5)')
+parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+parser.add_argument('--resume', action='store_true',
+                    help='Resume experiments from checkpoint directory')
+parser.add_argument('--seed', type=int, default=1337, help='RNG seed')
 args = parser.parse_args()
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -40,35 +44,39 @@ random.seed(args.seed)
 # create output directory tree:
 if os.path.isdir(args.name) and not args.resume:
     shutil.rmtree(args.name)
-logs_path = join(args.name,'logs')
-checkpoints_path = join(args.name,'checkpoints')
-checkpoint_path = join(checkpoints_path,'checkpoint.pth')
+logs_path = join(args.name, 'logs')
+checkpoints_path = join(args.name, 'checkpoints')
+checkpoint_path = join(checkpoints_path, 'checkpoint.pth')
 if not os.path.isdir(args.name):
     os.mkdir(args.name)
     os.mkdir(logs_path)
     os.mkdir(checkpoints_path)
 
 # record arguments for future reference:
-with open(join(args.name,'arguments.txt'),'w') as fout:
+with open(join(args.name, 'arguments.txt'), 'w') as fout:
     fout.write(str(args))
 
 # load metadata:
-metadata = pd.read_csv(join(args.data_dir,'gt.csv'))
+metadata = pd.read_csv(join(args.data_dir, 'gt.csv'))
 metadata.set_index('filename', inplace=True)
 
 # create datasets:
 toTensor = ToTensor()
-dataset_train = CellsDataset(args.data_dir,transform=ToTensor(),return_filenames=True)
+dataset_train = CellsDataset(
+    args.data_dir, transform=ToTensor(), return_filenames=True)
 
 random.shuffle(dataset_train.files)
-split_point = int(len(dataset_train) * 0.9) # 90/10 train/val split
-dataset_test = CellsDataset(args.data_dir,transform=ToTensor(),return_filenames=True)
+split_point = int(len(dataset_train) * 0.9)  # 90/10 train/val split
+dataset_test = CellsDataset(
+    args.data_dir, transform=ToTensor(), return_filenames=True)
 
 dataset_test.files = dataset_train.files[split_point:]
 dataset_train.files = dataset_train.files[:split_point]
 
-loader_train = DataLoader(dataset_train,batch_size=args.batchsize,shuffle=True,num_workers=4,pin_memory=True)
-loader_test =  DataLoader(dataset_test, batch_size=args.batchsize,shuffle=True,num_workers=4,pin_memory=True)
+loader_train = DataLoader(dataset_train, batch_size=args.batchsize,
+                          shuffle=True, num_workers=4, pin_memory=True)
+loader_test = DataLoader(dataset_test, batch_size=args.batchsize,
+                         shuffle=True, num_workers=4, pin_memory=True)
 
 
 # create model:
@@ -76,7 +84,8 @@ model = Convnet()
 model.to(device)
 
 # create optimizer:
-optimizer = torch.optim.Adam(model.parameters(),lr=args.lr,weight_decay=args.weight_decay)
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 if args.resume:
     try:
@@ -102,13 +111,15 @@ for epoch in range(15):
     model.train()
     for images, paths in tqdm(loader_train):
         images = images.to(device)
-        targets = torch.tensor([metadata['count'][os.path.split(path)[-1]] for path in paths]) # B
+        targets = torch.tensor(
+            [metadata['count'][os.path.split(path)[-1]] for path in paths])  # B
         targets = targets.float().to(device)
 
         # forward pass:
-        output = model(images) # B x 1 x 9 x 9 (analogous to a heatmap)
-        preds = output.sum(dim=[1,2,3]) # predicted cell counts (vector of length B)
-        
+        output = model(images)  # B x 1 x 9 x 9 (analogous to a heatmap)
+        # predicted cell counts (vector of length B)
+        preds = output.sum(dim=[1, 2, 3])
+
         # backward pass:
         loss = torch.mean((preds - targets)**2)
         optimizer.zero_grad()
@@ -118,38 +129,43 @@ for epoch in range(15):
         # logging:
         count_error = torch.abs(preds - targets).mean()
         writer.add_scalar('train_loss', loss.item(), global_step=global_step)
-        writer.add_scalar('train_count_error', count_error.item(), global_step=global_step)
+        writer.add_scalar('train_count_error',
+                          count_error.item(), global_step=global_step)
 
-        print("Step %d, loss=%f, count error=%f" % (global_step,loss.item(),count_error.item()))
+        print("Step %d, loss=%f, count error=%f" %
+              (global_step, loss.item(), count_error.item()))
 
         global_step += 1
-    
+
     mean_test_error = 0
     model.eval()
     for images, paths in tqdm(loader_test):
         images = images.to(device)
-        targets = torch.tensor([metadata['count'][os.path.split(path)[-1]] for path in paths]) # B
+        targets = torch.tensor(
+            [metadata['count'][os.path.split(path)[-1]] for path in paths])  # B
         targets = targets.float().to(device)
 
         # forward pass:
-        output = model(images) # B x 1 x 9 x 9 (analogous to a heatmap)
-        preds = output.sum(dim=[1,2,3]) # predicted cell counts (vector of length B)
+        output = model(images)  # B x 1 x 9 x 9 (analogous to a heatmap)
+        # predicted cell counts (vector of length B)
+        preds = output.sum(dim=[1, 2, 3])
 
         # logging:
         loss = torch.mean((preds - targets)**2)
         count_error = torch.abs(preds - targets).mean()
         mean_test_error += count_error
         writer.add_scalar('test_loss', loss.item(), global_step=global_step)
-        writer.add_scalar('test_count_error', count_error.item(), global_step=global_step)
-        
+        writer.add_scalar('test_count_error',
+                          count_error.item(), global_step=global_step)
+
         global_step += 1
-    
+
     mean_test_error = mean_test_error / len(loader_test)
     print("Test count error: %f" % mean_test_error)
     if mean_test_error < best_test_error:
         best_test_error = mean_test_error
-        torch.save({'state_dict':model.state_dict(),
-                    'optimizer_state_dict':optimizer.state_dict(),
-                    'globalStep':global_step,
-                    'train_paths':dataset_train.files,
-                    'test_paths':dataset_test.files},checkpoint_path)
+        torch.save({'state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'globalStep': global_step,
+                    'train_paths': dataset_train.files,
+                    'test_paths': dataset_test.files}, checkpoint_path)
